39c412651b50dfb2744e7de8e72ad579b2d55665
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2021-09-17 13:12:06 -0500

    Wands can now be accessed with the hotbar keys

--- Player.gd
+++ Player.gd
@@ -113,6 +113,19 @@ func _process(delta):
 	elif Input.is_action_just_released("scrolldown"):
 		Items.selected_wand = (Items.selected_wand + 1) % 6
 	
+	if Input.is_action_just_pressed("hotbar1"):
+		Items.selected_wand = 0
+	elif Input.is_action_just_pressed("hotbar2"):
+		Items.selected_wand = 1
+	elif Input.is_action_just_pressed("hotbar3"):
+		Items.selected_wand = 2
+	elif Input.is_action_just_pressed("hotbar4"):
+		Items.selected_wand = 3
+	elif Input.is_action_just_pressed("hotbar5"):
+		Items.selected_wand = 4
+	elif Input.is_action_just_pressed("hotbar6"):
+		Items.selected_wand = 5
+	
 
 
 func _physics_process(delta):
