5b0f0bb8d7947e920e86c117181d8b936750fc61
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2021-09-18 00:31:12 -0500

    Fixed a bug where you could pick an item from anywhere in the map

--- Items/ItemEntity.gd
+++ Items/ItemEntity.gd
@@ -2,13 +2,16 @@ extends RigidBody2D
 
 
 var item :Item
+var Player : KinematicBody2D
 
 func _ready():
 	$Sprite.texture = item.texture
+	Player = get_tree().get_nodes_in_group("Player")[0]
 
 func _process(delta):
 	$Sprite.rotation = -rotation
 	
-	if Input.is_action_just_pressed("down"):
-		Items.player_items.append(item.id)
-		queue_free()
+	if Player.position.distance_to(position) < 100:
+		if Input.is_action_just_pressed("down"):
+			Items.player_items.append(item.id)
+			queue_free()
