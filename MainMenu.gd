0e72be602228ca8728fcc6c0503eafbe8ce3253f
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2021-09-19 09:29:40 -0500

    Formatting

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,12 +1,8 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$Animations.play("License")
 
