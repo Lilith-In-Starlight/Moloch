4a777551e9bc8d31cb0cb87e7b07bbb23dcac632
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2021-09-22 00:01:58 -0500

    Added fire ball and ice ball

--- Spells/Iceball.gd
+++ Spells/Iceball.gd
@@ -5,22 +5,31 @@ var Caster :Node2D
 var goal :Vector2
 var frames := 0
 var rotate := 0.0
+var gravity := 0.0
 
 func _ready():
+	position = Caster.position
 	rotate = goal.angle_to_point(Caster.position)
+	if Caster.name == "Player":
+		set_collision_mask_bit(0, false)
 
 func _physics_process(delta):
+	gravity += delta
 	if frames < 3:
 		for body in $Area.get_overlapping_bodies():
 			if not body == self:
 				_on_body_entered(body)
+	else:
+		set_collision_mask_bit(0, true)
 	
 	
-	move_and_slide(Vector2(sin(rotate), cos(rotate))*600)
+	move_and_slide(Vector2(cos(rotate), sin(rotate)+gravity)*200)
 	frames += 1
 
 func _on_body_entered(body):
 	if body.has_method("health_object"):
-		if (is_instance_valid(Caster) and (body != Caster or frames >= 3)) or not is_instance_valid(Caster):
-			body.health_object().temp_change(-5.0 - randf() * 3.0)
-	queue_free()
+		if (is_instance_valid(Caster) and (body != Caster or frames >= 6)) or not is_instance_valid(Caster):
+			body.health_object().temp_change(-12.0 - randf() * 6.0)
+			queue_free()
+	elif body.is_in_group("World"):
+		queue_free()
