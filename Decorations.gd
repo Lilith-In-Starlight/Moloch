c8a5107a2f8852c767c719e6163d94d68b56b998
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2021-09-24 02:56:33 -0500

    Made more decorations

---
+++ Decorations.gd
@@ -0,0 +1,40 @@
+extends Node
+
+
+var paintings := {}
+var signs := {}
+
+func _ready():
+	register_sign(preload("res://Sprites/Elements/Decoration/Posters/Poster1.png"), "lawful")
+	register_sign(preload("res://Sprites/Elements/Decoration/Posters/Poster1.png"), "rebel")
+	register_sign(preload("res://Sprites/Elements/Decoration/Posters/Poster1_ripped.png"), "rebel")
+	register_sign(preload("res://Sprites/Elements/Decoration/Posters/Poster1_rotten.png"), "rebel", true)
+	register_sign(preload("res://Sprites/Elements/Decoration/Posters/Poster1_rotten.png"), "lawful", true)
+	
+	register_painting(preload("res://Sprites/Elements/Decoration/Paintings/painting1.png"), "lawful")
+	register_painting(preload("res://Sprites/Elements/Decoration/Paintings/leader_vandalized4.png"), "rebel")
+	register_painting(preload("res://Sprites/Elements/Decoration/Paintings/leader_vandalized.png"), "rebel")
+	register_painting(preload("res://Sprites/Elements/Decoration/Paintings/leader_vandalized3.png"), "rebel")
+	register_painting(preload("res://Sprites/Elements/Decoration/Paintings/leader_vandalized2.png"), "rebel")
+	register_painting(preload("res://Sprites/Elements/Decoration/Paintings/leader_rotten.png"), "lawful", true)
+	register_painting(preload("res://Sprites/Elements/Decoration/Paintings/leader_vandalized_rotten.png"), "rebel", true)
+
+func register_painting(painting:Texture, type :String, mushroom := false):
+	if not type in paintings:
+		paintings[type] = {true:[], false:[]}
+	paintings[type][mushroom].append(painting)
+	
+func register_sign(painting:Texture, type :String, mushroom := false):
+	if not type in signs:
+		signs[type] = {true:[], false:[]}
+	signs[type][mushroom].append(painting)
+
+func get_painting(type:String, mushroom := false) -> Texture:
+	if type in paintings:
+		return paintings[type][mushroom][Items.WorldRNG.randi()%paintings[type][mushroom].size()]
+	return null
+	
+func get_sign(type:String, mushroom := false) -> Texture:
+	if type in signs:
+		return signs[type][mushroom][Items.WorldRNG.randi()%signs[type][mushroom].size()]
+	return null
---
+++ RoomDecorations/PosterGroup.gd
@@ -0,0 +1,22 @@
+tool
+extends Position2D
+
+export var maximum := 5
+export(RectangleShape2D) var shape:RectangleShape2D
+
+func _ready():
+	if not Engine.editor_hint:
+		set_process(false)
+		for i in Items.WorldRNG.randi()%maximum:
+			var n := preload("res://RoomDecorations/Poster.tscn").instance()
+			n.position = global_position - shape.extents/2.0*8.0
+			n.position += Vector2(Items.WorldRNG.randf(), Items.WorldRNG.randf())*shape.extents*8.0
+			n.z_index = -1
+			get_parent().get_parent().call_deferred("add_child", n)
+
+func _process(delta):
+	if Engine.editor_hint and shape != null:
+		update()
+
+func _draw():
+	draw_rect(Rect2(-shape.extents/2.0*8.0, shape.extents*8.0), ColorN("dark_green", 0.5))
