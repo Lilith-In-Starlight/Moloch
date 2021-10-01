05754ee53e77006f8bcd3b7594411016f5254748
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2021-10-01 13:37:01 -0500

    Improved the Incomplete's ability to track the player

--- Enemies/Incomplete.gd
+++ Enemies/Incomplete.gd
@@ -11,6 +11,9 @@ var Player :KinematicBody2D
 var speed := Vector2(0, 0)
 var noise := OpenSimplexNoise.new()
 var first_check := false
+var last_seen := Vector2(0, 0)
+var search_time := 0.0
+
 
 func _ready():
 	noise.seed = hash(self)
@@ -20,6 +23,9 @@ func _ready():
 
 
 func _physics_process(delta):
+	var frames := Engine.get_frames_drawn()
+	$Senses.cast_to = speed
+	$Eye.cast_to = (Player.position-position).normalized()*500
 	if not first_check:
 		if Player.position.distance_to(position) < 500:
 			queue_free()
@@ -29,13 +35,29 @@ func _physics_process(delta):
 				queue_free()
 		first_check = true
 	
-	var primordial_tremor := Vector2(noise.get_noise_2d(position.x, Engine.get_frames_drawn()), noise.get_noise_2d(position.y, Engine.get_frames_drawn()))*5
+	var primordial_tremor := Vector2(noise.get_noise_2d(position.x, frames), noise.get_noise_2d(position.y, frames))*5
 	match state:
 		STATES.NORMAL:
-			if Player.position.distance_to(position) > 500:
-				speed = primordial_tremor
+			if $Eye.is_colliding() and $Eye.get_collider() == Player:
+				var goal := (position - Player.position).normalized()*130 + Player.position
+				speed = lerp(speed, primordial_tremor*30 + (goal - position), 0.2)
+				last_seen = Player.position
 			else:
-				var goal := (position - Player.position).normalized()*80 + Player.position
-				speed = primordial_tremor + (goal-position).normalized()
-			speed = move_and_slide(speed*60)
+				speed = lerp(speed, primordial_tremor*60, 0.2)
+				if last_seen != Vector2(0, 0):
+					state = STATES.SEARCHING
+					search_time = 0.0
+			speed = move_and_slide(speed)
+		
+		STATES.SEARCHING:
+			search_time += delta
+			if $Eye.is_colliding() and $Eye.get_collider() == Player:
+				state = STATES.NORMAL
+			else:
+				speed = lerp(speed, primordial_tremor*20 + (last_seen - position), 0.2)
+			
+			if search_time > 12.0:
+				state = STATES.NORMAL
+				last_seen = Vector2(0, 0)
+			speed = move_and_slide(speed)
 
