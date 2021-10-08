2c04da9c131f473d77891234c2b6471bf92795a0
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2021-10-07 23:15:38 -0500

    Added variable to know whether player is using controller

--- Autoload/Config.gd
+++ Autoload/Config.gd
@@ -3,6 +3,9 @@ extends Node
 
 var config_file:ConfigFile = ConfigFile.new()
 
+# Controller Support
+var last_input_was_controller := false
+
 # Accessibility
 var damage_visuals := false
 var instant_death_button := false
@@ -15,9 +18,19 @@ func _ready() -> void:
 		instant_death_button = config_file.get_value("config", "instant_death_button", false)
 	else:
 		save_config()
+	
+	if Input.get_connected_joypads().size() > 0:
+		last_input_was_controller = true
 
 
 func save_config() -> void:
 	config_file.set_value("config", "damage_visuals", damage_visuals)
 	config_file.set_value("config", "instant_death_button", instant_death_button)
 	config_file.save("user://config.moloch")
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		last_input_was_controller = false
+	elif event is InputEventJoypadButton or event is InputEventJoypadMotion:
+		last_input_was_controller = true
