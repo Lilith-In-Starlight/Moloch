32bb93117bbd9cddf27bac42d29935153d0ad834
Katie And <katie-and@protonmail.com>
2021-10-17 09:44:21 -0500

    Added the Internal Rage spell

--- Particles/Explosion.gd
+++ Particles/Explosion.gd
@@ -3,21 +3,27 @@ extends Area2D
 var done := 0
 var radius := 70
 var timer := 0.0
+var dmg_multi := 1.0
+var area_of_effect := 10
 
 var Map
 
 func _ready():
-	Map = get_tree().get_nodes_in_group("World")[0] 
-	for x in range(-10,11):
-		for y in range(-10,11):
+	$CollisionShape2D.shape = $CollisionShape2D.shape.duplicate(true)
+	Map = get_tree().get_nodes_in_group("World")[0]
+	dmg_multi = area_of_effect/10.0 
+	radius *= dmg_multi
+	$CollisionShape2D.shape.radius *= dmg_multi
+	for x in range(-area_of_effect,area_of_effect+1):
+		for y in range(-area_of_effect,area_of_effect+1):
 			var vec := Vector2(x+int(position.x/8), y+int(position.y/8))
-			if Vector2(x,y).length()<=7+randi()%4:
+			if Vector2(x,y).length()<=area_of_effect*0.7+randi()%4:
 				Map.set_cellv(vec, Items.break_block(Map.get_cellv(vec), 0.5))
 	var point := Vector2(int(position.x/8), int(position.y/8))
-	Map.update_bitmask_region(point-Vector2(10,10), point+Vector2(10,10))
+	Map.update_bitmask_region(point-Vector2(area_of_effect,area_of_effect), point+Vector2(area_of_effect,area_of_effect))
 
 func _process(delta):
-	radius = clamp(lerp(radius, 0, -1.5), 0, 70)
+	radius = clamp(lerp(radius, 0, -1.5), 0, 70*dmg_multi)
 	timer += delta
 	if timer > 0.5:
 		queue_free()
@@ -31,17 +37,17 @@ func _physics_process(_delta):
 		for body in get_overlapping_bodies():
 			if body.has_method("health_object"):
 				var flesh : Flesh = body.health_object()
-				flesh.temp_change(500/body.position.distance_to(position))
-				if body.position.distance_to(position) < 24:
+				flesh.temp_change(500*dmg_multi/body.position.distance_to(position))
+				if body.position.distance_to(position) < 24*dmg_multi:
 					flesh.poke_hole(2+randi()%1)
-				if body.position.distance_to(position) < 12:
+				if body.position.distance_to(position) < 12*dmg_multi:
 					flesh.poke_hole(5+randi()%10)
-				if body.position.distance_to(position) < 8:
+				if body.position.distance_to(position) < 8*dmg_multi:
 					flesh.poke_hole(10+randi()%20)
 				if body.get("speed"):
-					body.speed += (body.position - position)*100
+					body.speed += (body.position - position)*100*dmg_multi
 				elif body.get("linear_velocity"):
-					body.linear_velocity += (position - body.position).normalized()*200/(position.distance_squared_to(body.position))
+					body.linear_velocity += (position - body.position).normalized()*200*dmg_multi/(position.distance_squared_to(body.position))
 		done += 1
 	else:
 		queue_free()
---
+++ Spells/InternalRage.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+
+var thing := []
+var CastInfo := SpellCastInfo.new()
+
+
+func _ready() -> void:
+	CastInfo.set_position(self)
+	var next_distance := 500
+	for i in get_children():
+		var angle := randf()*TAU
+		i.position = Vector2(cos(angle), sin(angle))*next_distance
+		thing.append([0, i.position.angle()])
+		next_distance += 300 + randf()*400
+
+
+func _process(delta: float) -> void:
+	var queue := []
+	for i in get_child_count():
+		var child := get_child(i)
+		var dis :float = child.position.length()
+		thing[i][0] += 0.5*delta*60
+		var new_pos :Vector2 = child.position.normalized()*(dis-thing[i][0])
+		if child.position.angle() - new_pos.angle() < 0.0001:
+			child.position = new_pos
+		else:
+			pass
+			queue.append([child, i])
+			var n := preload("res://Particles/Explosion.tscn").instance()
+			n.area_of_effect = 20
+			n.position = position
+			get_parent().add_child(n)
+	for i in queue:
+		i[0].queue_free()
+		thing.remove(i[1])
