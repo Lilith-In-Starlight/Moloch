fa5ee5cf8d25ad3f2b177b271047785d3c423066
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2021-10-24 15:17:14 -0500

    Added the Short Ray spell

---
+++ Spells/Shortray.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+
+onready var Ray := $Ray1
+onready var Line := $Line2D
+
+
+var CastInfo := SpellCastInfo.new()
+var angle := 0.0
+
+
+func _ready() -> void:
+	var Map :TileMap = get_tree().get_nodes_in_group("World")[0]
+	CastInfo.set_position(self)
+	angle = CastInfo.get_angle(self)
+	Map.play_sound(preload("res://Sfx/spells/laserfire01.wav"), position, 1.0, 0.8+randf()*0.4)
+	 
+	var point := Vector2(cos(angle), sin(angle))*100.0
+	Ray.cast_to = point
+	Ray.force_raycast_update()
+	if Ray.is_colliding():
+		point = Ray.get_collision_point() - position
+		var collider :Node2D = Ray.get_collider()
+		if collider.has_method("health_object"):
+			collider.health_object().shatter_soul(0.2)
+			if randf() < 0.25:
+				collider.poke_hole()
+	Line.points = [Vector2(0, 0), point]
+
+
+func _on_Timer_timeout() -> void:
+	queue_free()
