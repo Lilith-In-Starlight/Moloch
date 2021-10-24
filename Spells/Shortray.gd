2390d6f37e37bb28be818fad7f75167a4ebcfd0c
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2021-10-24 18:20:12 -0500

    Fixed an issue where the shortray would crash the game

--- Spells/Shortray.gd
+++ Spells/Shortray.gd
@@ -24,7 +24,7 @@ func _ready() -> void:
 		if collider.has_method("health_object"):
 			collider.health_object().shatter_soul(0.2)
 			if randf() < 0.25:
-				collider.poke_hole()
+				collider.health_object().poke_hole()
 	Line.points = [Vector2(0, 0), point]
 
 
