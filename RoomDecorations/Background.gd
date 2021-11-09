9734f4c878aa9e9b444f25345dcf7587753a66e8
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2021-11-09 05:45:06 -0500

    The background tile size is no longer larger than it needs to be

--- RoomDecorations/Background.gd
+++ RoomDecorations/Background.gd
@@ -4,6 +4,8 @@ extends CanvasLayer
 onready var Tiles := $Following
 var Cam :Camera2D
 
+const TILE_SIZE := Vector2(400, 224)
+
 func _ready():
 	Cam = get_tree().get_nodes_in_group("Camera")[0]
 
@@ -13,8 +15,8 @@ func _process(_delta):
 	var y := -1
 	var i := 0
 	for Tile in Tiles.get_children():
-		Tile.rect_position.x = stepify(cam_pos.x, 400)+400*x
-		Tile.rect_position.y = stepify(cam_pos.y, 225)+225*y - i
+		Tile.rect_position.x = stepify(cam_pos.x, TILE_SIZE.x)+TILE_SIZE.x*x
+		Tile.rect_position.y = stepify(cam_pos.y, TILE_SIZE.y)+TILE_SIZE.y*y
 		x += 1
 		if x > 1:
 			i += 1
