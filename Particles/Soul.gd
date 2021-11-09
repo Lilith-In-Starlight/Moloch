576b432178319e75b05766515fe03877b06f9599
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2021-11-08 22:32:41 -0500

    Fixed an issue where soul jitter particles wasn't frame-independant

--- Particles/Soul.gd
+++ Particles/Soul.gd
@@ -2,8 +2,8 @@ extends Sprite
 
 
 func _process(delta: float) -> void:
-	position.y += -3.0+randf()*6.0*delta*60.0
-	position.x += -3.0+randf()*+6.0*delta*60.0
+	position.y += (-3.0+randf()*6.0)*delta*60.0
+	position.x += (-3.0+randf()*+6.0)*delta*60.0
 
 
 func _on_Timer_timeout() -> void:
