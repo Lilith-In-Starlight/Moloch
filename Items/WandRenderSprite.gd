bd9261554eb731528ab58496bb45bd65c0853c23
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2021-11-13 13:25:35 -0500

    Fixed the textures of the wand according to the new maximum spell capacity

--- Items/WandRenderSprite.gd
+++ Items/WandRenderSprite.gd
@@ -22,20 +22,20 @@ func render_wand(wand:Wand, selected :bool = false):
 		$Base.texture = preload("res://Sprites/Wands/Base.png")
 		if not wand.shuffle:
 			match wand.spell_capacity:
-				1: $Slots.texture = preload("res://Sprites/Wands/Model1/1.png")
-				2: $Slots.texture = preload("res://Sprites/Wands/Model1/2.png")
-				3: $Slots.texture = preload("res://Sprites/Wands/Model1/3.png")
-				4: $Slots.texture = preload("res://Sprites/Wands/Model1/4.png")
-				5: $Slots.texture = preload("res://Sprites/Wands/Model1/5.png")
-				6: $Slots.texture = preload("res://Sprites/Wands/Model1/6.png")
+				1, 2: $Slots.texture = preload("res://Sprites/Wands/Model1/1.png")
+				3, 4: $Slots.texture = preload("res://Sprites/Wands/Model1/2.png")
+				5, 6: $Slots.texture = preload("res://Sprites/Wands/Model1/3.png")
+				7, 8: $Slots.texture = preload("res://Sprites/Wands/Model1/4.png")
+				9, 10: $Slots.texture = preload("res://Sprites/Wands/Model1/5.png")
+				11, 12: $Slots.texture = preload("res://Sprites/Wands/Model1/6.png")
 		else:
 			match wand.spell_capacity:
-				1: $Slots.texture = preload("res://Sprites/Wands/Shuffle1/1.png")
-				2: $Slots.texture = preload("res://Sprites/Wands/Shuffle1/2.png")
-				3: $Slots.texture = preload("res://Sprites/Wands/Shuffle1/3.png")
-				4: $Slots.texture = preload("res://Sprites/Wands/Shuffle1/4.png")
-				5: $Slots.texture = preload("res://Sprites/Wands/Shuffle1/5.png")
-				6: $Slots.texture = preload("res://Sprites/Wands/Shuffle1/6.png")
+				1, 2: $Slots.texture = preload("res://Sprites/Wands/Shuffle1/1.png")
+				3, 4: $Slots.texture = preload("res://Sprites/Wands/Shuffle1/2.png")
+				5, 6: $Slots.texture = preload("res://Sprites/Wands/Shuffle1/3.png")
+				7, 8: $Slots.texture = preload("res://Sprites/Wands/Shuffle1/4.png")
+				9, 10: $Slots.texture = preload("res://Sprites/Wands/Shuffle1/5.png")
+				11, 12: $Slots.texture = preload("res://Sprites/Wands/Shuffle1/6.png")
 		match 6-int(round((wand.spell_recharge/0.3)*6)):
 			0, 1: $Cast.texture = preload("res://Sprites/Wands/CastSpeed/1.png")
 			2: $Cast.texture = preload("res://Sprites/Wands/CastSpeed/2.png")
