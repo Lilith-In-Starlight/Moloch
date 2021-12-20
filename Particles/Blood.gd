5dc8072cb1561673f019e7243c3c221622d35a42
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2021-12-20 18:59:11 -0500

    Lava blood can now burn entities

--- Particles/Blood.gd
+++ Particles/Blood.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 
 var timer := 0.0
+var substance := "blood"
 
 func _physics_process(delta):
 	timer += delta
@@ -9,3 +10,8 @@ func _physics_process(delta):
 	if timer > 0.5:
 		modulate.a -= 0.05
 	$Polygon2D.rotation = linear_velocity.angle()
+	
+	if substance == "lava":
+		for i in $Area2D.get_overlapping_bodies():
+			if i.has_method("health_object"):
+				i.health_object().temperature += 10.0
