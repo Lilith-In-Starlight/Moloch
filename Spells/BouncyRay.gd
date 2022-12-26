2e9dadf813eceeb4454959b3b94fe319bc8abe3f
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2022-12-26 15:23:23 -0500

    RayBehavior is now the part of the spell that does the raycasting

--- Spells/BouncyRay.gd
+++ Spells/BouncyRay.gd
@@ -1,4 +1,4 @@
-extends RayCast2D
+extends Node2D
 
 
 var CastInfo := SpellCastInfo.new()
@@ -12,43 +12,62 @@ var did := false
 func _ready():
 	add_child(spell_behavior)
 	spell_behavior.ray_setup(self, 2000)
-	CastInfo.set_position(self)
-	CastInfo.set_goal()
-	spell_behavior.get_angle(CastInfo.goal + CastInfo.goal_offset, position, CastInfo)
-	cast_to = spell_behavior.get_cast_to(CastInfo)
-	position += spell_behavior.get_cast_to(CastInfo).normalized()
+	spell_behavior.connect("hit_something", self, "_on_hit_something", [], 4)
+	spell_behavior.connect("hit_nothing", self, "_on_hit_nothing", [], 4)
+	spell_behavior.cast(CastInfo)
+	
 	var Map :TileMap = get_tree().get_nodes_in_group("World")[0]
 	Map.play_sound(preload("res://Sfx/spells/laserfire01.wav"), position, 1.0, 0.8+randf()*0.4)
 
 
-func _physics_process(delta):
-	timer += delta
-	if is_colliding():
-		var pos := get_collision_point()
-		if get_collider().has_method("health_object") and not did:
-			get_collider().health_object().poke_hole(1, CastInfo.Caster)
-			did = true
-		$RayCast2D.points = [Vector2(0, 0), pos-position]
-		if times_done < 12 and timer > 0.18 and not casted:
-			casted = true
-			var new = load("res://Spells/BouncyRay.tscn").instance()
-			new.CastInfo.Caster = self
-			get_parent().add_child(new)
-			enabled = false
-			new.times_done = times_done + 1
-	else:
-		$RayCast2D.points[1] = spell_behavior.get_cast_to(CastInfo)
-	if timer > 0.2:
-		queue_free()
-
-
 func looking_at():
-	if is_colliding():
-		var pos := get_collision_point()
-		var normal := get_collision_normal()
+	if spell_behavior.is_colliding():
+		var pos :Vector2 = spell_behavior.get_collision_point()
+		var normal :Vector2 = spell_behavior.get_collision_normal()
+		print(normal)
 		return (pos - position).bounce(normal.normalized())*20 + pos
+	else:
+		return spell_behavior.cast_to
 
 
 func cast_from():
-	if is_colliding():
-		return get_collision_point()
+	if spell_behavior.is_colliding():
+		return spell_behavior.get_collision_point() - spell_behavior.cast_to.normalized() * 3
+
+
+func _on_hit_something():
+	var pos :Vector2 = spell_behavior.get_collision_point()
+	if spell_behavior.get_collider().has_method("health_object") and not did:
+		spell_behavior.get_collider().health_object().poke_hole(1, CastInfo.Caster)
+		did = true
+	$RayCast2D.points = [Vector2(0, 0), pos-position]
+	var new_timer := Timer.new()
+	new_timer.wait_time = 0.2
+	new_timer.autostart = true
+	if times_done < 12:
+		new_timer.connect("timeout", self, "_on_new_bounce_timeout")
+	else:
+		new_timer.connect("timeout", self, "_on_death_timeout")
+		
+	add_child(new_timer)
+
+func _on_hit_nothing():
+	var new_timer := Timer.new()
+	new_timer.wait_time = 0.2
+	new_timer.autostart = true
+	new_timer.connect("timeout", self, "_on_death_timeout")
+	add_child(new_timer)
+	$RayCast2D.points[1] = spell_behavior.cast_to
+
+
+func _on_new_bounce_timeout():
+	var new = load("res://Spells/BouncyRay.tscn").instance()
+	new.CastInfo.Caster = self
+	new.times_done = times_done + 1
+	get_parent().add_child(new)
+	queue_free()
+
+
+func _on_death_timeout():
+	queue_free()
+	
--- Spells/SpellBehaviors/Ray.gd
+++ Spells/SpellBehaviors/Ray.gd
@@ -1,14 +1,22 @@
-extends Node
+extends RayCast2D
 
 
 class_name RayBehavior
 
 signal hit_something
-signal never_hit_something
+signal hit_nothing
 
 var angle := 0.0
 
 var length := 2000
+
+
+func _ready() -> void:
+	set_collision_mask_bit(0, true)
+	set_collision_mask_bit(1, true)
+	set_collision_mask_bit(3, true)
+	set_collision_mask_bit(4, true)
+	set_collision_mask_bit(6, true)
 
 
 func ray_setup(entity: Node2D, ray_length: float):
@@ -28,8 +36,16 @@ func get_angle(start: Vector2, end: Vector2, cast_info: SpellCastInfo):
 	return output
 
 
-func get_cast_to(cast_info: SpellCastInfo) -> Vector2:
+func get_cast_to_from_cast_info(cast_info: SpellCastInfo) -> Vector2:
 	if cast_info.modifiers.has("limited"):
 		cast_info.vector_from_angle(angle, 2)
 	return cast_info.vector_from_angle(angle, length)
 	
+
+func cast(cast_info: SpellCastInfo):
+	cast_to = get_cast_to_from_cast_info(cast_info)
+	force_raycast_update()
+	if is_colliding():
+		emit_signal("hit_something")
+	else:
+		emit_signal("hit_nothing")
