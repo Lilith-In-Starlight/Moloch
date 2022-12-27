abc8d93188cbdd8387801df441922758f5f3a40c
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2022-12-26 19:26:52 -0500

    Wand mixers now destroy the room if you're greedy

---
+++ Particles/Earthquake.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+func _ready() -> void:
+	for i in get_children():
+		i.cast_to = Vector2(1, 0).rotated(-randf() * PI) * 30*8
+		print(i.cast_to.normalized())
+		print(i.cast_to.angle())
+		i.force_raycast_update()
+		if i.is_colliding():
+			var n: RigidBody2D = preload("res://Particles/EarthquakeDebris.tscn").instance()
+			n.position = i.get_collision_point() + i.cast_to.normalized() * 8
+			n.apply_central_impulse((n.position - position) * 5)
+			get_parent().add_child(n)
