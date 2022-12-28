ad58b71fa4a66ad2e6ae62c9de04020a6dddb268
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2022-12-28 14:17:46 -0500

    Soul Wave now has AreaBehavior

--- Spells/SoulWave.gd
+++ Spells/SoulWave.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 
 var CastInfo := SpellCastInfo.new()
+var spell_behavior := AreaBehavior.new()
 var shape := CircleShape2D.new()
 
 var frames := 0
@@ -14,6 +15,7 @@ func _ready() -> void:
 	$CollisionShape2D.shape = shape
 	$SoulWave.scale.x = shape.radius / 250.0
 	$SoulWave.scale.y = shape.radius / 250.0
+	spell_behavior.projectile_side.velocity = spell_behavior.get_initial_velocity(self)
 	
 	Map.play_sound(preload("res://Sfx/spells/BellSound.wav"), position, 1.0, 0.8+randf()*0.4)
 
@@ -24,6 +26,7 @@ func _physics_process(delta: float) -> void:
 	$SoulWave.scale.x = shape.radius / 250.0
 	$SoulWave.scale.y = shape.radius / 250.0
 	modulate.a -= delta / 2.0
+	position += spell_behavior.projectile_side.move(0.0, CastInfo) * delta * 60
 	for i in get_overlapping_bodies():
 		if i.has_method("health_object"):
 			var dist :float = i.position.distance_to(position)
