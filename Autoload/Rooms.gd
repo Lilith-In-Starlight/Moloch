5210098890c0abbbcf2b2a06e6774ff51c3a913f
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2022-12-29 21:03:46 -0500

    Make room model loader code more readable

--- Autoload/Rooms.gd
+++ Autoload/Rooms.gd
@@ -5,17 +5,17 @@ var rooms := []
 
 func _ready():
 	print("Loading all room models")
-	var dir := Directory.new()
-	dir.open("res://Rooms/Sacrifice")
-	dir.list_dir_begin(true, true)
+	var rooms_directory := Directory.new()
+	rooms_directory.open("res://Rooms/Sacrifice")
+	rooms_directory.list_dir_begin(true, true)
 	while true:
-		var file := dir.get_next()
-		if file != "":
-			var di := "res://Rooms/Sacrifice/" + file
-			if di.find("Begin") == -1 and di.find("End") == -1:
-				print(di)
-				var c = load(di)
-				rooms.append(c)
-		else:
+		var room_filename := rooms_directory.get_next()
+		if room_filename == "":
 			break
+		var room_full_path := "res://Rooms/Sacrifice/" + room_filename
+		
+		if room_full_path.find("Begin") == -1 and room_full_path.find("End") == -1:
+			print(room_full_path)
+			var room_packed_scene = load(room_full_path)
+			rooms.append(room_packed_scene)
 	
