da36401e7ef529e90daf86e99ed1d7a02096d151
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2022-12-30 02:41:09 -0500

    Vague reimplementation of the minimap

--- HUD/Minimap.gd
+++ HUD/Minimap.gd
@@ -12,31 +12,29 @@ func _process(_delta):
 
 func _draw():
 	var door_points := []
-	for i in get_tree().get_nodes_in_group("World")[0].areas:
-		var r :Rect2= i
+	for i in get_tree().get_nodes_in_group("World")[0].world_tile_instances:
+		var r :Rect2 = Rect2(i * Rooms.tile_size * 8 + Vector2.ONE * 16, Rooms.tile_size * 8 - Vector2.ONE * 32)
 		r.position = r.position/16.0/4.0-get_tree().get_nodes_in_group("Player")[0].position/16.0/4.0
 		r.size = r.size/16.0/4.0
 		if r.has_point(Vector2(0, 0)) or been_to.has(i):
 			draw_rect(r, ColorN("white"), true)
 			if not i in drawn_edge_to:
 				drawn_edge_to.append(i)
-				for j in get_tree().get_nodes_in_group("World")[0].areas:
-					if j in edges or j in been_to:
-						continue
-					var r2 :Rect2= j
-					r2.position = r2.position/16.0/4.0-get_tree().get_nodes_in_group("Player")[0].position/16.0/4.0
-					r2.size = r2.size/16.0/4.0
-					var r3 := r
-					r3.position -= Vector2.ONE*1
-					r3.size += Vector2.ONE*2
-					if r2.intersects(r3, true):
-						edges.append(j)
+				if get_tree().get_nodes_in_group("World")[0].world_tile_instances[i].top != -1 and not i + Vector2.UP in edges:
+					edges.append(i + Vector2.UP)
+				if get_tree().get_nodes_in_group("World")[0].world_tile_instances[i].left != -1 and not i + Vector2.LEFT in edges:
+					edges.append(i + Vector2.LEFT)
+				if get_tree().get_nodes_in_group("World")[0].world_tile_instances[i].bottom != -1 and not i + Vector2.DOWN in edges:
+					edges.append(i + Vector2.DOWN)
+				if get_tree().get_nodes_in_group("World")[0].world_tile_instances[i].right != -1 and not i + Vector2.RIGHT in edges:
+					edges.append(i + Vector2.RIGHT)
 			if not been_to.has(i):
 				been_to.append(i)
 			else:
 				edges.erase(i)
 	for i in edges:
-		var r:Rect2 = i
+		var v:Vector2 = i
+		var r:Rect2 = Rect2(i * Rooms.tile_size * 8 + Vector2.ONE * 16, Rooms.tile_size * 8 - Vector2.ONE * 32)
 		r.position = r.position/16.0/4.0-get_tree().get_nodes_in_group("Player")[0].position/16.0/4.0
 		r.size = r.size/16.0/4.0
 		draw_rect(r, ColorN("white"), false)
