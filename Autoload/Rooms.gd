d30f5fa2b276733e074a395eba5a9bce38c86606
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2022-12-29 21:15:30 -0500

    Prepared room model loader for tile-based generation

--- Autoload/Rooms.gd
+++ Autoload/Rooms.gd
@@ -1,6 +1,15 @@
 extends Node
 
-var rooms := []
+var all_tiles := []
+
+var tiles_by_side := {
+	"top" : {-1: [], 0: [], 1: [], 2: []},
+	"bottom" : {-1: [], 0: [], 1: [], 2: []},
+	"left" : {-1: [], 0: [], 1: [], 2: []},
+	"right" : {-1: [], 0: [], 1: [], 2: []},
+}
+
+var tile_size := Vector2(16, 9)
 
 
 func _ready():
@@ -8,14 +17,28 @@ func _ready():
 	var rooms_directory := Directory.new()
 	rooms_directory.open("res://Rooms/Sacrifice")
 	rooms_directory.list_dir_begin(true, true)
+	
 	while true:
 		var room_filename := rooms_directory.get_next()
+		
 		if room_filename == "":
 			break
+			
 		var room_full_path := "res://Rooms/Sacrifice/" + room_filename
 		
 		if room_full_path.find("Begin") == -1 and room_full_path.find("End") == -1:
-			print(room_full_path)
-			var room_packed_scene = load(room_full_path)
-			rooms.append(room_packed_scene)
+			var new_tile_index = all_tiles.size()
+			print(new_tile_index, ": ", room_full_path)
+			
+			var room_packed_scene: PackedScene = load(room_full_path)
+			all_tiles.append(room_packed_scene)
+			
+			var room_instance = room_packed_scene.instance()
+			
+			tiles_by_side["left"][room_instance.left].append(new_tile_index)
+			tiles_by_side["top"][room_instance.top].append(new_tile_index)
+			tiles_by_side["bottom"][room_instance.bottom].append(new_tile_index)
+			tiles_by_side["right"][room_instance.right].append(new_tile_index)
+			
+			room_instance.queue_free()
 	
