65c7f4c6a52e1df4f4a58bdc0c33ec2d6e29506e
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2022-12-30 12:41:41 -0500

    Changes to the enemies

--- World.gd
+++ World.gd
@@ -212,7 +212,7 @@ func add_enemies():
 	var enemies = []
 	for tile in world_tile_instances:
 		var area = Rect2(tile * Rooms.tile_size * 8, Rooms.tile_size * 8)
-		for i in Items.WorldRNG.randi()%4:
+		for i in Items.WorldRNG.randi()%3:
 			enemies.append(add_enemy_with_chance(area, preload("res://Enemies/MagicDrone.tscn"), 0.7))
 			enemies.append(add_enemy_with_chance(area, preload("res://Enemies/SpellMachine.tscn"), 0.3))
 			enemies.append(add_enemy_with_chance(area, preload("res://Enemies/Incomplete.tscn"), 0.1))
