bf440d652d02c39af2e57a0ac3eb4f663c04fd42
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2022-12-30 21:41:19 -0500

    Removed unnecessary text set

--- HUD/Pause.gd
+++ HUD/Pause.gd
@@ -11,7 +11,6 @@ func _ready() -> void:
 	$Settings/InstantDeathButton.pressed = Config.instant_death_button
 	$Settings/JoystickSensitivity/Text.text = "Joystick Sensitivity: " + str(Config.joystick_sensitivity)
 	$Settings/JoystickSensitivity.value = Config.joystick_sensitivity
-	$Seed.text = "Seed: " + str(Items.using_seed)
 
 
 func _on_DieInstantly_pressed() -> void:
