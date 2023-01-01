1d4017acf1f70941ed76daf69dfa8b58510770a1
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2023-01-01 04:05:36 -0500

    Changed most of the main menu to use godot's builtin UI functionality

--- MainMenu.gd
+++ MainMenu.gd
@@ -19,12 +19,8 @@ const MolochTheme := preload("res://Themes/Theme.tres")
 onready var Animations := $Animations
 onready var SeedLineEdit := $MainMenu/LineEdit
 
-var current_menu := "" setget set_menu
 
-
-var menus := {}
-
-var menu_positions := {}
+var menu := "main_menu"
 
 var viewing_achievements := false
 
@@ -36,135 +32,72 @@ var random_noise := OpenSimplexNoise.new()
 
 
 func _ready():
-	menus = {
-		"main": [
-			make_menu_option("New Run", "", funcref(self, "start_new_run")),
-			make_menu_option("Achievements", "", funcref(self, "show_achievements")),
-			make_menu_option("Settings", "settings"),
-			make_menu_option("Exit", "", funcref(get_tree(), "quit")),
-			],
-		"settings": [
-			make_menu_setting("Death Button", Config.instant_death_button, "idb"),
-			make_menu_setting("Damage Colors", Config.damage_visuals, "dv"),
-			make_menu_setting("Accessible Font", Config.use_accessible_font, "uaf"),
-			make_menu_setting("Joystick Sensitivity", Config.joystick_sensitivity, "joys"),
-			make_menu_setting("Camera Sensitivity", Config.camera_smoothing, "camsense"),
-			make_menu_option("Controls", "controls"),
-			make_menu_option("Back", "main"),
-			],
-		"controls": [
-			make_menu_option("Up:", "", funcref(self, "start_changing_key"), ["up"]),
-			make_menu_option("Down:", "", funcref(self, "start_changing_key"), ["down"]),
-			make_menu_option("Left:", "", funcref(self, "start_changing_key"), ["left"]),
-			make_menu_option("Right:", "", funcref(self, "start_changing_key"), ["right"]),
-			make_menu_option("Jump/Select:", "", funcref(self, "start_changing_key"), ["jump"]),
-			make_menu_option("Shoot/Select:", "", funcref(self, "start_changing_key"), ["Interact1"]),
-			make_menu_option("Drop:", "", funcref(self, "start_changing_key"), ["Interact2"]),
-			make_menu_option("Seal Wounds:", "", funcref(self, "start_changing_key"), ["seal_blood"]),
-			make_menu_option("See Item Info:", "", funcref(self, "start_changing_key"), ["see_info"]),
-			make_menu_option("Instantly Die:", "", funcref(self, "start_changing_key"), ["instant_death"]),
-			make_menu_option("Proceed", "", funcref(self, "proceed_keybinds")),
-			make_menu_option("Regret And Go Back", "settings"),
-			]
-	}
-	set_menu("main")
 	Animations.play("License")
+	$MainMenuContainer/NewRunButton.grab_focus()
+	$SettingsMenuContainer/AccessibleFontCheckbox.pressed = Config.use_accessible_font
+	$SettingsMenuContainer/DamageColorsCheckbox.pressed = Config.damage_visuals
+	$SettingsMenuContainer/MouseSensitivityContainer/Slider.value = Config.camera_smoothing
 
 
 func _process(delta: float) -> void:
 	$Symbol/SymbolGlow.modulate.a = 0.1 + random_noise.get_noise_1d(Engine.get_frames_drawn() * 3.0) * 0.05
 	$Symbol.material.set_shader_param("var", 1.0 + random_noise.get_noise_1d(Engine.get_frames_drawn() * 3.0) * 0.05)
-	var menu_element:Control = $MenuContainer.get_child(current_menu_pos())
 	$Symbol/Smoke.texture.noise_offset.y += 0.5
 	$Symbol/Smoke.texture.noise.persistence = 0.6 + sin(Engine.get_frames_drawn() * 0.0001) * 0.1
 	$Symbol/Smoke.texture.noise.lacunarity = 1.9 + sin(Engine.get_frames_drawn() * 0.0001) * 0.2
 	
-	$Ball.rect_position = lerp($Ball.rect_position, menu_element.rect_global_position - Vector2(12,  -menu_element.rect_size.y / 2.0 + 6), 0.5)
-	Config.instant_death_button = menus["settings"][0].enabled
-	Config.damage_visuals = menus["settings"][1].enabled
-	
-	$MainMenu/LineEdit.visible = current_menu == "main"
-	
-	if current_menu == "controls":
-		$MainMenu.modulate.a = lerp($MainMenu.modulate.a, 0.0, 0.2)
-		$MainMenu.rect_scale = lerp($MainMenu.rect_scale, Vector2(0.8, 0.8), 0.2)
-		$MainMenu.rect_position = lerp($MainMenu.rect_position, $MainMenu.rect_size*0.1, 0.2)
-		$MenuContainer.rect_position.y = lerp($MenuContainer.rect_position.y, 8, 0.2)
-		if $MainMenu.modulate.a < 0.01:
-			$MainMenu.visible = false
-		if changed_keys.empty():
-			$MenuContainer.get_child(menus["controls"].size() - 2).modulate = ColorN("gray")
-		else:
-			$MenuContainer.get_child(menus["controls"].size() - 2).modulate = ColorN("white")
-		for i in menus["controls"].size() - 2:
-			var Child := $MenuContainer.get_child(i)
-			Child.text = menus["controls"][i].option_name + " " + get_action_text(menus["controls"][i].func_args[0])
-			if changing_key == menus["controls"][i].func_args[0]:
-				Child.modulate = "#0055ff"
-			else:
-				Child.modulate = "#ffffff"
-	else:
-		$MainMenu.modulate.a = lerp($MainMenu.modulate.a, 1.0, 0.2)
-		$MainMenu.rect_scale = lerp($MainMenu.rect_scale, Vector2(1.0, 1.0), 0.2)
-		$MainMenu.rect_position = lerp($MainMenu.rect_position, Vector2(0.0, 0.0), 0.2)
-		$MenuContainer.rect_position.y = lerp($MenuContainer.rect_position.y, 105, 0.2)
-		$Achievements.visible = viewing_achievements
-		$MainMenu.visible = !viewing_achievements
-		$MenuContainer.visible = !viewing_achievements
-		$Ball.visible = !viewing_achievements
-	
 
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.is_pressed():
-		SeedLineEdit.release_focus()
-	if changing_key == "":
-		if (event is InputEventKey or event is InputEventJoypadButton) and event.is_pressed():
-			var current_selection = menus[current_menu][current_menu_pos()]
-			var action := ""
-			if not SeedLineEdit.has_focus() or event is InputEventJoypadButton:
-				for i in ["up", "down", "left", "right", "jump"]:
-					for j in ["", "scroll", "scroll_"]:
-						if InputMap.has_action(j + i) and Input.is_action_just_pressed(j + i):
-							action = i
-							break
-			else:
-				if event is InputEventKey and event.scancode == KEY_ESCAPE:
-					SeedLineEdit.release_focus()
-			if not viewing_achievements:
-				match action:
-					"up":
-						set_current_pos(current_menu_pos() - 1)
-						if current_menu_pos() < 0:
-							set_current_pos(menus[current_menu].size()-1)
-					"down":
-						set_current_pos((current_menu_pos() + 1) % menus[current_menu].size())
-					"jump":
-						if current_selection is MenuOption:
-							if current_selection.menu_dest != "":
-								set_menu(current_selection.menu_dest)
-								current_menu_pos()
-							elif current_selection.func_ref != null:
-								if current_selection.func_args.empty():
-									current_selection.func_ref.call_func()
-								else:
-									current_selection.func_ref.call_funcv(current_selection.func_args)
-						else:
-							if not current_selection.slider:
-								toggle($MenuContainer.get_child(current_menu_pos()), current_selection)
-					"left":
-						if current_selection is MenuSetting:
-							slide($MenuContainer.get_child(current_menu_pos()), current_selection, -1)
-					"right":
-						if current_selection is MenuSetting:
-							slide($MenuContainer.get_child(current_menu_pos()), current_selection, 1)
-			else:
-				if (event is InputEventKey and not event.scancode in [KEY_ALT, KEY_SUPER_L, KEY_MASK_META, KEY_SHIFT, KEY_S, KEY_W]) or event is InputEventJoypadButton:
-					viewing_achievements = false
-	else:
-		if (event is InputEventKey or event is InputEventMouseButton) and event.is_pressed():
-			changed_keys[changing_key] = event
-			changing_key = ""
+#func _input(event: InputEvent) -> void:
+#	if event is InputEventMouseButton and event.is_pressed():
+#		SeedLineEdit.release_focus()
+#	if changing_key == "":
+#		if (event is InputEventKey or event is InputEventJoypadButton) and event.is_pressed():
+#			var current_selection = menus[current_menu][current_menu_pos()]
+#			var action := ""
+#			if not SeedLineEdit.has_focus() or event is InputEventJoypadButton:
+#				for i in ["up", "down", "left", "right", "jump"]:
+#					for j in ["", "scroll", "scroll_"]:
+#						if InputMap.has_action(j + i) and Input.is_action_just_pressed(j + i):
+#							action = i
+#							break
+#			else:
+#				if event is InputEventKey and event.scancode == KEY_ESCAPE:
+#					SeedLineEdit.release_focus()
+#			if not viewing_achievements:
+#				match action:
+#					"up":
+#						set_current_pos(current_menu_pos() - 1)
+#						if current_menu_pos() < 0:
+#							set_current_pos(menus[current_menu].size()-1)
+#					"down":
+#						set_current_pos((current_menu_pos() + 1) % menus[current_menu].size())
+#					"jump":
+#						if current_selection is MenuOption:
+#							if current_selection.menu_dest != "":
+#								set_menu(current_selection.menu_dest)
+#								current_menu_pos()
+#							elif current_selection.func_ref != null:
+#								if current_selection.func_args.empty():
+#									current_selection.func_ref.call_func()
+#								else:
+#									current_selection.func_ref.call_funcv(current_selection.func_args)
+#						else:
+#							if not current_selection.slider:
+#								toggle($MenuContainer.get_child(current_menu_pos()), current_selection)
+#					"left":
+#						if current_selection is MenuSetting:
+#							slide($MenuContainer.get_child(current_menu_pos()), current_selection, -1)
+#					"right":
+#						if current_selection is MenuSetting:
+#							slide($MenuContainer.get_child(current_menu_pos()), current_selection, 1)
+#			else:
+#				if (event is InputEventKey and not event.scancode in [KEY_ALT, KEY_SUPER_L, KEY_MASK_META, KEY_SHIFT, KEY_S, KEY_W]) or event is InputEventJoypadButton:
+#					viewing_achievements = false
+#	else:
+#		if (event is InputEventKey or event is InputEventMouseButton) and event.is_pressed():
+#			changed_keys[changing_key] = event
+#			changing_key = ""
 
 
 func proceed_keybinds():
@@ -178,47 +111,6 @@ func proceed_keybinds():
 		InputMap.action_add_event(action, new_event)
 	changed_keys = {}
 	Config.save_config()
-
-
-func set_menu(menu:String) -> void:
-	current_menu = menu
-	changed_keys = {}
-	for i in $MenuContainer.get_children():
-		i.queue_free()
-	var count := -1
-	for i in menus[menu]:
-		count += 1
-		if i is MenuOption:
-			var new_button := ToolButton.new()
-			new_button.text = i.option_name
-			new_button.theme = MolochTheme
-			$MenuContainer.add_child(new_button)
-			new_button.align = ToolButton.ALIGN_CENTER
-			new_button.connect("mouse_entered", self, "set_selection_to", [count])
-			new_button.connect("pressed", self, "reset_focus", [new_button])
-			if i.menu_dest != "":
-				new_button.connect("pressed", self, "set_menu", [i.menu_dest])
-			else:
-				new_button.connect("pressed", i.func_ref, "call_func", i.func_args)
-		elif i is MenuSetting:
-			if i.slider:
-				var new_button := ToolButton.new()
-				new_button.text = i.setting_name + ": " + str(i.value)
-				new_button.theme = MolochTheme
-				$MenuContainer.add_child(new_button)
-				new_button.align = ToolButton.ALIGN_CENTER
-				new_button.connect("mouse_entered", self, "set_selection_to", [count])
-				new_button.connect("pressed", self, "slide", [new_button, i, 1])
-				new_button.connect("pressed", self, "reset_focus", [new_button])
-			else:
-				var new_check := CheckBox.new()
-				new_check.text = i.setting_name
-				new_check.pressed = i.enabled
-				new_check.theme = MolochTheme
-				$MenuContainer.add_child(new_check)
-				new_check.connect("mouse_entered", self, "set_selection_to", [count])
-				new_check.connect("pressed", self, "toggle", [new_check, i])
-				new_check.connect("pressed", self, "reset_focus", [new_check])
 
 
 func start_new_run() -> void:
@@ -298,11 +190,10 @@ func change_config_setting(config_setting:String, setting:MenuSetting):
 
 
 func show_achievements():
-	viewing_achievements = true
-
-
-func set_selection_to(value:int) -> void:
-	set_current_pos(value)
+	$MainMenuContainer.visible = false
+	$Achievements.visible = true
+	$MainMenu.visible = false
+	$Achievements/MainMenuButton.grab_focus()
 
 
 func start_changing_key(key) -> void:
@@ -346,12 +237,38 @@ func reset_focus(Child:Control) -> void:
 	Child.release_focus()
 
 
-func current_menu_pos():
-	if not current_menu in menu_positions:
-		menu_positions[current_menu] = 0
-	return menu_positions[current_menu]
+func quit_game():
+	get_tree().quit()
 
 
-func set_current_pos(new_pos:int) -> void:
-	current_menu_pos()
-	menu_positions[current_menu] = new_pos
+func set_to_main_menu() -> void:
+	$MainMenu.visible = true
+	$MainMenuContainer.visible = true
+	if $Achievements.visible:
+		$Achievements.visible = false
+		$MainMenuContainer/AchievementsButton.grab_focus()
+	elif $SettingsMenuContainer.visible:
+		$SettingsMenuContainer.visible = false
+		$MainMenuContainer/SettingsButton.grab_focus()
+
+
+func view_settings() -> void:
+	$MainMenuContainer.visible = false
+	$SettingsMenuContainer.visible = true
+	$MainMenu.visible = false
+	$SettingsMenuContainer/MouseSensitivityContainer/Slider.grab_focus()
+
+
+func set_damage_colors(button_pressed: bool) -> void:
+	Config.damage_visuals = button_pressed
+	Config.save_config()
+
+
+func set_accessible_font(button_pressed: bool) -> void:
+	Config.use_accessible_font = button_pressed
+	Config.save_config()
+
+
+func set_mouse_sensitivity(value: float) -> void:
+	Config.camera_smoothing = value
+	Config.save_config()
