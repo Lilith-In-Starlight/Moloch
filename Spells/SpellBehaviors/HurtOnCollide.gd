2eda114747f7568856806a3fe449b022b15ffc92
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2023-09-19 02:25:46 -0500

    Only poke if it will add one or more holes

--- Spells/SpellBehaviors/HurtOnCollide.gd
+++ Spells/SpellBehaviors/HurtOnCollide.gd
@@ -10,6 +10,7 @@ var caster :Node = null
 
 func _on_collision_happened(collider: Node, collision_point: Vector2, collision_normal: Vector2):
 	if collider.has_method("health_object"):
-		collider.health_object().poke_hole(poke_holes, caster)
+		if poke_holes != 0:
+			collider.health_object().poke_hole(poke_holes, caster)
 		collider.health_object().shatter_soul(soul_damage, caster)
 
