c15714cca1ddd1a30091ba11222aabf5ed90e11d
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2023-09-21 13:17:10 -0500

    HurtCasterModule won't attempt to hurt a non-existing entity

--- Spells/SpellBehaviors/HurtCasterModule.gd
+++ Spells/SpellBehaviors/HurtCasterModule.gd
@@ -7,11 +7,13 @@ var caster :Node = null
 
 
 func change_temp(deg: float) -> void:
+	if not is_instance_valid(caster): return
 	if caster.has_method("health_object"):
 		caster.health_object().temp_change(deg, caster, true)
 
 
 func push(force: Vector2) -> void:
+	if not is_instance_valid(caster): return
 	if caster.get("speed"):
 		caster.speed += force
 	elif caster.get("linear_velocity"):
