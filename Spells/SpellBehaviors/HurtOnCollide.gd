bf5834f27128f8b5e01d0017f9b36546808083e4
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2023-09-23 10:39:23 -0500

    HurtOnCollide can now apply status effects

--- Spells/SpellBehaviors/HurtOnCollide.gd
+++ Spells/SpellBehaviors/HurtOnCollide.gd
@@ -7,6 +7,7 @@ var soul_damage := 0.0
 var poke_holes := 0
 var heat_damage := 0.0
 var caster :Node = null
+var effects :Array = []
 
 
 func _on_collision_happened(collider: Node, collision_point: Vector2, collision_normal: Vector2):
@@ -20,4 +21,6 @@ func _on_collision_happened(collider: Node, collision_point: Vector2, collision_
 		collider.health_object().poke_hole(poke_holes, caster)
 	collider.health_object().shatter_soul(soul_damage, caster)
 	collider.health_object().temp_change(heat_damage, caster)
+	for i in effects:
+		collider.health_object().add_effect(i)
 
