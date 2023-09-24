a01477bccf074171758232816c5d1580cd63bd41
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2023-09-24 13:10:24 -0500

    Bleeding now uses the new Flesh system

--- Entities/Bleed.gd
+++ Entities/Bleed.gd
@@ -7,16 +7,18 @@ extends Node2D
 
 
 func _process(delta: float) -> void:
-	var health = get_parent().health_object()
+	var health: Flesh = get_parent().health_object()
+	if not health.body_module or not health.blood_module:
+		return
 	# If the player is bleeding
-	if health.poked_holes > 0 and health.blood > 0.01:
-		if health.blood_substance == "water" and "onfire" in health.effects:
+	if health.body_module.holes > 0 and health.blood_module.amount > 0.01:
+		if health.blood_module.substance == "water" and "onfire" in health.effects:
 			health.effects.erase("onfire")
 		# Emit blood particles 
-		for i in min(health.poked_holes, 12):
+		for i in min(health.body_module.holes, 12):
 			if randf()>0.9:
 				var n :RigidBody2D = preload("res://Particles/Blood.tscn").instance()
 				n.position = global_position + Vector2(0, 6)
 				n.linear_velocity = Vector2(-200 + randf()*400, -80 + randf()*120)
-				n.substance = health.blood_substance
+				n.substance = health.blood_module.substance
 				get_parent().get_parent().add_child(n)
