d6132bb1b7064ffc8fce01525a5872926bae14df
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2023-09-25 16:13:36 -0500

    Fix typo in SoulEater spell

--- Spells/SoulEater.gd
+++ Spells/SoulEater.gd
@@ -28,7 +28,7 @@ func _on_hit_something():
 	if col.has_method("health_object") and not did and col.health_object().soul_module.amount > 0.0:
 		did = true
 		if is_instance_valid(CastInfo.Caster):
-			var shatter := min(col.health_object().sou_module.amount, 0.025)
+			var shatter := min(col.health_object().soul_module.amount, 0.025)
 			CastInfo.Caster.health_object().shatter_soul(-shatter)
 			col.health_object().shatter_soul(shatter, CastInfo.Caster)
 	elif not did:
