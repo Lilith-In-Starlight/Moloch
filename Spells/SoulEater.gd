812d27e7db92f2eadfeb4ed2cbe802c709262ffe
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2023-09-28 13:09:07 -0500

    Port Soul Eater to the new spell system

--- Spells/SoulEater.gd
+++ Spells/SoulEater.gd
@@ -1,41 +1,51 @@
-extends Node2D
+extends SpellManager
 
 
-var CastInfo := SpellCastInfo.new()
-var spell_behavior := RayBehavior.new()
+var rotate := 0.0
+var WorldMap :Node2D
+
 var timer := 0.0
-var did := false
+
+var noise := OpenSimplexNoise.new()
+
+
 
 func _ready():
-	add_child(spell_behavior)
-	spell_behavior.ray_setup(self, 1000)
-	spell_behavior.connect("hit_something", self, "_on_hit_something", [], 4)
-	spell_behavior.connect("hit_nothing", self, "_on_hit_nothing", [], 4)
-	var Map :Node2D = get_tree().get_nodes_in_group("World")[0]
-	Map.play_sound(preload("res://Sfx/spells/laserfire01.wav"), position, 1.0, 0.8+randf()*0.4)
-
-func _physics_process(delta):
-	timer += delta
 	CastInfo.set_position(self)
-	spell_behavior.cast(CastInfo)
+	CastInfo.set_goal()
+	movement_manager = ParicleMovement.new()
+	movement_manager.use_wand_speed = false
+	movement_manager.shape = Items.player_hitbox
+	movement_manager.max_bounces = 1
+	movement_manager.gravity = 0.0
+	movement_manager.velocity = (CastInfo.goal - position).normalized() * 5000
+	movement_manager.set_up_to(self)
+	add_child(movement_manager)
+	movement_manager.connect("collision_happened", self, "_on_collision_happened_2")
 	
-	if timer > 0.05:
-		queue_free()
+	side_effects = HurtCasterModule.new()
+	side_effects.caster = CastInfo.Caster
+	add_child(side_effects)
+	
+	
+	movement_manager.connect("request_movement", $Trail, "_on_request_movement")
+	
+	var sound_emitter := AudioStreamPlayer2D.new()
+	sound_emitter.stream = preload("res://Sfx/spells/laserfire01.wav")
+	sound_emitter.position = position
+	sound_emitter.pitch_scale = 0.9 + float()*0.3
+	get_parent().add_child(sound_emitter)
+	sound_emitter.play()
 
 
-func _on_hit_something():
-	var col = spell_behavior.get_collider()
-	if col.has_method("health_object") and not did and col.health_object().soul_module and col.health_object().soul_module.amount > 0.0:
-		did = true
-		if is_instance_valid(CastInfo.Caster):
-			var shatter := min(col.health_object().soul_module.amount, 0.025)
-			CastInfo.Caster.health_object().shatter_soul(-shatter)
-			col.health_object().shatter_soul(shatter, CastInfo.Caster)
-	elif not did:
-		CastInfo.drain_caster_soul(0.05)
-		did = true
-	$Line2D.points = [Vector2(0, 0), spell_behavior.get_collision_point() - position]
-
-
-func _on_hit_nothing():
-	$Line2D.points = [Vector2(0, 0), spell_behavior.cast_to]
+func _on_collision_happened_2(collider: Node2D, _point, _normal):
+	if collider.has_method("health_object"):
+		if is_instance_valid(CastInfo.Caster) and CastInfo.Caster.has_method("health_object") and CastInfo.Caster.health_object().soul_module:
+			CastInfo.Caster.health_object().soul_module.amount += 0.1
+			collider.health_object().shatter_soul(0.1)
+			$Trail.color = Color("#25ff00")
+			$Trail.recolor_trail()
+	else:
+		CastInfo.drain_caster_soul(0.1)
+		$Trail.color = Color("#e23a00")
+		$Trail.recolor_trail()
