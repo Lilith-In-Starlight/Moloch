0b1e8d9e8c728e17431e51c71978ddfdbb7c9afa
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2023-09-30 13:03:26 -0500

    SpellSpawner can now spawn based on spell entities

--- Spells/SpellBehaviors/SpellSpawner.gd
+++ Spells/SpellBehaviors/SpellSpawner.gd
@@ -4,7 +4,8 @@ class_name SpellSpawner
 
 signal finished()
 
-var spell :PackedScene
+var spell :PackedScene = null
+var spell_object :Spell
 var interval := 0.02
 var amount := 16
 var rotation := 0.0
@@ -13,17 +14,25 @@ var use_spell_as_caster := false
 var global_position : Vector2
 
 
+
 func spawn():
-	for i in amount:
-		global_position = get_parent().global_position
-		var new_spell := spell.instance()
-		new_spell.CastInfo = get_parent().CastInfo.duplicate()
-		if use_spell_as_caster:
-			new_spell.CastInfo.position_caster = self
-		get_parent().get_parent().add_child(new_spell)
-		current_rotation += rotation
-		if amount > 1:
-			yield(get_tree().create_timer(interval), "timeout")
+	if spell_object == null:
+		var game_node = get_tree().get_nodes_in_group("GameNode")[0]
+		for i in amount:
+			game_node._on_casting_spell(spell_object, get_parent().CastInfo.wand, get_parent().CastInfo.caster)
+			if amount > 1:
+				yield(get_tree().create_timer(interval), "timeout")
+	elif spell != null:
+		for i in amount:
+			global_position = get_parent().global_position
+			var new_spell := spell.instance()
+			new_spell.CastInfo = get_parent().CastInfo.duplicate()
+			if use_spell_as_caster:
+				new_spell.CastInfo.position_caster = self
+			get_parent().get_parent().add_child(new_spell)
+			current_rotation += rotation
+			if amount > 1:
+				yield(get_tree().create_timer(interval), "timeout")
 	emit_signal("finished")
 
 
