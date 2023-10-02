b323e194377238f91851169e336c6ad284f51e8c
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2023-10-02 10:46:42 -0500

    Change health limits of the fire dude

--- FiremanProperties.gd
+++ FiremanProperties.gd
@@ -23,5 +23,11 @@ func _ready() -> void:
 	health.add_body()
 	health.add_temperature()
 	health.add_soul()
+	health.body_module.max_holes = 24
+	health.soul_module.maximum = 2.0
+	health.soul_module.amount = 2.0
+	health.temperature_module.max_temperature = 244
+	health.temperature_module.min_temperature = 10
+	health.temperature_module.temperature = 60
 	health.connect("died", get_parent(), "_on_died")
 	add_child(health)
